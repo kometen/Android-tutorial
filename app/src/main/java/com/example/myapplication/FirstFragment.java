b459f52f9f1cab4394de223d34ef26a8f24a22fe
Claus Guttesen <kometen@gmail.com>
2023-02-21 19:17:54 +0100

    Rearranged gradle.

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -11,13 +11,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.myapplication.FirstFragmentDirections;
 import com.example.myapplication.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
-    TextView showCountTextView;
+    //TextView showCountTextView;
 
     @Override
     public View onCreateView(
@@ -49,11 +50,13 @@ public class FirstFragment extends Fragment {
         });
 
         binding.randomButton.setOnClickListener(new View.OnClickListener() {
-            //int currentCount = Integer.parseInt(showCountTextView.getText().toString());
             @Override
             public void onClick(View view) {
+                int currentCount = Integer.parseInt(binding.textviewFirst.getText().toString());
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action =
+                        FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(action);
             }
         });
     }
